c825f86bf0a2ed6f23ecd2ab96a5eaaf9938ddff
cebrailblox <eymencebrail@icloud.com>
2024-02-27 11:21:02 +0300

    Initial Commit

---
+++ SocialMediaUIYT/SocialMediaUIYTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaUIYTApp.swift
+//  SocialMediaUIYT
+//
+//  Created by EYHAN on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaUIYTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
